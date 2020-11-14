90af75fe44f31fe0c7b5591160544e810f43d71f
Pokahonta <kristine.tene@gmail.com>
2020-11-14 09:55:35 +0200

    html+session

---
+++ src/main/java/com/app/controllers/RestUserController.java
@@ -0,0 +1,24 @@
+package com.app.controllers;
+
+
+import com.app.model.User;
+import com.app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestUserController {
+
+    @Autowired
+    UserService userService;
+
+    @CrossOrigin
+    @GetMapping("/getUsers")
+    public List<User> getUsers(){
+        return userService.getAllUsers();
+    }
+}
